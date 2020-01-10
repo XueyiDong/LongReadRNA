f2b082bea085c660b1bfc114b61d5ad12e7f9176
Xueyi Dong <dong.x@wehi.edu.au>
2020-01-10 17:06:44 +1100

    changed compare.R

--- long/scFLT/gffcompare/compare.R
+++ long/scFLT/gffcompare/compare.R
@@ -3,6 +3,6 @@ library(ggplot2)
 pdf("compare_category.pdf", height = 4)
 ggplot(tracking, aes(x = V4, fill=V4)) +
   geom_bar() +
-  geom_text(stat="count", aes(label=..count..), vjust=-1) +
+  geom_text(stat="count", aes(label=..count..), vjust=-0.2) +
   theme_bw()
 dev.off()
