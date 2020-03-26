6d192d83bb73ee033df46c75b2760cbfab109dc2
Xueyi Dong <dong.x@wehi.edu.au>
2020-03-26 16:35:34 +1100

    added analysis of sequins data

--- long/run2/QC/fulllength.R
+++ long/run2/QC/fulllength.R
@@ -22,18 +22,20 @@ for (i in 1:12){
   saveRDS(summ, file=file.path(dir_bam, paste0(bams[i], ".summ.RDS")))
 }
 
-# read in RDS
+# read in RDS (no fresia)
 
 readDF <- lapply(c(1, 4, 5, 6, 7, 9, 10, 11), function(x){
   readRDS(file.path(dir_bam, paste0(bams[x], "readDF.RDS")))})
 DF <- rbind(readDF[[1]], readDF[[2]], readDF[[3]], readDF[[4]],
             readDF[[5]], readDF[[6]], readDF[[7]], readDF[[8]])
-DF$sample <- rep(c(1, 2, 3, 4, 1, 5, 6, 7), 
+DF$sample <- rep(c("1", "2", "3", "4", "1", "5", "6", "7"), 
                  c(nrow(readDF[[1]]), nrow(readDF[[2]]), nrow(readDF[[3]]),
                    nrow(readDF[[4]]), nrow(readDF[[5]]), nrow(readDF[[6]]),
                    nrow(readDF[[7]]), nrow(readDF[[8]])))
 saveRDS(DF, file=file.path(dir_bam, "readDF.all.RDS"))
 rm(readDF)
+
+DF <- readRDS(file.path(dir_bam, "readDF.all.RDS"))
 
 
 # read GFF and calculate tx len
@@ -68,55 +70,50 @@ DF$tx_len <- tl$tx_len[m]
 
 # make plots
 
-maxLength = max(readDF$tx_len)
-readDF$txLengthGroup <- cut2(readDF$tx_len, cuts = c(0, 500, 1000, 1500, 
+maxLength = max(DF$tx_len)
+DF$txLengthGroup <- cut2(DF$tx_len, cuts = c(0, 500, 1000, 1500, 
                                                      2000, maxLength))
-readDF$covFraction <- readDF$width / readDF$tx_len
-readDF$known <- grepl("^ENSMUST", readDF$seqnames)
+DF$covFraction <- DF$width / DF$tx_len
+DF$known <- grepl("^ENSMUST", DF$seqnames)
+DF$group[DF$sample %in% c("1", "4", "5", "6")] <- "wt"
+DF$group[DF$sample %in% c("2", "3", "7")] <- "Smchd1"
 
 
-
-pdf("fullLengthViolin.pdf")
+pdf("fullLengthViolinLen.pdf")
 # tx length
-ggplot(readDF, aes(x=txLengthGroup, y=covFraction)) +
+ggplot(DF, aes(x=txLengthGroup, y=covFraction)) +
   geom_violin() +
-  theme_bw()
+  theme_bw() +
+  ylim(0, 1) 
+dev.off()
+pdf("fullLengthViolinNovelty.pdf")
 # tx length and novelty
-ggplot(readDF, aes(x=txLengthGroup, y=covFraction, fill = txLengthGroup)) +
+ggplot(DF, aes(x=txLengthGroup, y=covFraction, fill = txLengthGroup)) +
   geom_violin() +
-  theme_bw()
-# sample, fill by group
-ggplot(readDF, aes(x=sample, y=covFraction, fill=group)) +
+  theme_bw() +
+  ylim(0, 1) 
+dev.off()
+pdf("fullLengthViolinSample.pdf")
+# sample, filled by group
+ggplot(DF, aes(x=sample, y=covFraction, fill=group)) +
   geom_violin() +
-  theme_bw()
-# group, fill by group
-ggplot(readDF, aes(x=group, y=covFraction, fill=group)) +
-  geom_violin() +
-  theme_bw()
+  theme_bw() +
+  ylim(0, 1) +
+  scale_fill_brewer(palette="Set2")
+dev.off()
+pdf("fullLengthBoxSample.pdf")
+# sample, filled by group
+ggplot(DF, aes(x=sample, y=covFraction, fill=group)) +
+  geom_boxplot() +
+  theme_bw() +
+  ylim(0, 1) +
+  scale_fill_brewer(palette="Set2")
 dev.off()
 
-# plots wo fresia
+# look into why some cov fraction > 1
 
-readDF <- readDF[readDF$group == "wt" | readDF$group == "smchd1",]
-save(readDF, file = "readDF_filtered.RData")
-
-pdf("fullLengthViolinNoFr.pdf")
-# tx length
-ggplot(readDF, aes(x=txLengthGroup, y=covFraction)) +
-  geom_violin() +
-  theme_bw()
-# tx length and novelty
-ggplot(readDF, aes(x=txLengthGroup, y=covFraction, fill = txLengthGroup)) +
-  geom_violin() +
-  theme_bw()
-# sample, fill by group
-ggplot(readDF, aes(x=sample, y=covFraction, fill=group)) +
-  geom_violin() +
-  theme_bw()
-# group, fill by group
-ggplot(readDF, aes(x=group, y=covFraction, fill=group)) +
-  geom_violin() +
-  theme_bw()
-dev.off()
-
-
+covFracLargerThan1 <- DF[DF$covFraction>1, ]
+head(covFracLargerThan1)
+dim(covFracLargerThan1)
+length(unique(covFracLargerThan1$read))
+length(unique(covFracLargerThan1$seqnames))
