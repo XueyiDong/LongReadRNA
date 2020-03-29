bbbeec8c606ee2290de879c3cd8a8c18a601d494
Xueyi Dong <dong.x@wehi.edu.au>
2020-03-29 16:19:47 +1100

    updated sequins compare script

--- long/sequins/compare.R
+++ long/sequins/compare.R
@@ -1,6 +1,6 @@
-tracking <- read.table("scFLT.tracking", stringsAsFactors = FALSE)
+tracking <- read.table("comp.tracking", stringsAsFactors = FALSE)
 library(ggplot2)
-pdf("compare_category.pdf", height = 4)
+pdf("compare_category_sequins.pdf", height = 4)
 ggplot(tracking, aes(x = V4, fill=V4)) +
   geom_bar() +
   geom_text(stat="count", aes(label=..count..), vjust=-0.2) +
@@ -9,16 +9,17 @@ dev.off()
 
 # barplot by  read number but not number of transcripts
 
-count_file <- "../run1/transcript_count.csv"
-count <- read.csv(count_file, stringsAsFactors = FALSE)
+count <-read.csv("/stornext/General/data/user_managed/grpu_mritchie_1/SCmixology/Mike_seqin/20200228_YPRDP_2xsequin_mixAB/FLTSA_output/transcript_count.csv")
 
 info <- strsplit2(tracking$V5, "|", fixed=TRUE)
 tracking$transcript <- sub("transcript:", "", info[,2])
-count$all <- rowSums(count[,3:16])
+count$all <- rowSums(count[,3:ncol(count)])
 count$category <- tracking$V4[match(count$transcript_id, tracking$transcript)]
 head(count)
-known <- grepl("^ENSMUST", count$transcript_id)
-count$category[known] <- "="
+# known <- grepl("^ENSMUST", count$transcript_id)
+# count$category[known] <- "="
+count[(is.na(count$category)),]
+count[(is.na(count$category)), "category"] = "="
 table(count$category)
 count_cat <- aggregate(count$all, by = list(count$category), FUN=sum)
 colnames(count_cat) <- c("category", "count")
