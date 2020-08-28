adce7812acaa6e8333efed93dd3634bebf4ca8e4
XueyiDong <dong.x@slurm-login01.hpc.wehi.edu.au>
2020-08-28 18:04:52 +1000

    updated scripts for flair and flames for sequins

--- sequins/flair/run_flair.sh
+++ sequins/flair/run_flair.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#PBS -N flair_collapse
-#PBS -q submit
-#PBS -l nodes=1:ppn=4,mem=40gb,walltime=24:00:00
-#PBS -M dong.x@wehi.edu.au
-#PBS -m abe
-#PBS -j oe
+#SBATCH --time=48:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=100G
+#SBATCH --mail-user=dong.x@wehi.edu.au
+#SBATCH --mail-type=BEGIN,END,FAIL
 
 
 export PATH=$PATH:/wehisan/home/allstaff/d/dong.x/Programs/minimap2
@@ -15,26 +14,26 @@ module load anaconda3
 source activate
 conda activate flair
 
-cd /wehisan/home/allstaff/d/dong.x/analysis/smchd1/long/sequins/flair
+cd /wehisan/home/allstaff/d/dong.x/analysis/2020/smchd1/sequins/flair
 anno=/wehisan/home/allstaff/d/dong.x/annotation/sequins
-reads=/stornext/General/data/user_managed/grpu_mritchie_1/SCmixology/Mike_seqin/20200228_YPRDP_2xsequin_mixAB/FLTSA_output/merged_fq.fastq.gz
+reads=/stornext/General/data/user_managed/grpu_mritchie_1/SCmixology/Mike_seqin/20200228_YPRDP_2xsequin_mixAB/sequins_rebasecall/pass/merged
 flairdir=/wehisan/home/allstaff/d/dong.x/Programs/flair
 
 # align
-# python $flairdir/flair.py align -g $anno/rnasequin_decoychr_2.4.fa -r $reads -t 4
+python $flairdir/flair.py align -g $anno/rnasequin_decoychr_2.4.fa -r $reads/barcode01.fq.gz $reads/barcode02.fq.gz $reads/barcode03.fq.gz $reads/barcode04.gz -t 8 -v1.3
 
 # # correct 
-# python $flairdir/flair.py correct -q flair.aligned.bed -f $anno/rnasequin_annotation_2.4.gtf -g $anno/rnasequin_decoychr_2.4.fa -t 4
+python $flairdir/flair.py correct -q flair.aligned.bed -f $anno/rnasequin_annotation_2.4.gtf -g $anno/rnasequin_decoychr_2.4.fa -t 8
 
 # # collapse
-# python $flairdir/flair.py collapse -g $anno/rnasequin_decoychr_2.4.fa -r $reads -q flair_all_corrected.bed -t 8 --temp_dir ./tmp
+python $flairdir/flair.py collapse -g $anno/rnasequin_decoychr_2.4.fa -f $anno/rnasequin_annotation_2.4.gtf -r $reads/barcode01.fq.gz $reads/barcode02.fq.gz $reads/barcode03.fq.gz $reads/barcode04.gz -q flair_all_corrected.bed -t 8 --temp_dir ./tmp
 
 # # quantify
-# python $flairdir/flair.py quantify -r reads_manifest.tsv -i flair.collapse.isoforms.fa -t 8 --temp_dir ./tmp
+python $flairdir/flair.py quantify -r reads_manifest.tsv -i flair.collapse.isoforms.fa -t 8 --temp_dir ./tmp
 
 # # collapse again
 # reads_rmbc=/stornext/General/data/user_managed/grpu_mritchie_1/SCmixology/Mike_seqin/20200228_YPRDP_2xsequin_mixAB/fastq_pass/merged/all.fastq
 # python $flairdir/flair.py collapse -g $anno/rnasequin_decoychr_2.4.fa -r $reads_rmbc -q flair_all_corrected.rmScBc.bed -t 8 --temp_dir ./tmp -o flair.collapse.rmScBc
 
 # quantify
-python $flairdir/flair.py quantify -r reads_manifest.tsv -i flair.collapse.rmScBc.isoforms.fa -t 4 --temp_dir ./tmp
+# python $flairdir/flair.py quantify -r reads_manifest.tsv -i flair.collapse.rmScBc.isoforms.fa -t 4 --temp_dir ./tmp
--- sequins/flames/run_flames.sh
+++ sequins/flames/run_flames.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#SBATCH --partition=submit
-#SBATCH --ntasks=8
-#SBATCH --mem-per-cpu=100GB
-#SBATCH --time=120:00:00
+#SBATCH --time=48:00:00
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=100G
 #SBATCH --mail-user=dong.x@wehi.edu.au
 #SBATCH --mail-type=BEGIN,END,FAIL
 
@@ -18,11 +17,12 @@ FLAMES=/wehisan/home/allstaff/d/dong.x/Programs/FLAMES
 anno=/wehisan/home/allstaff/d/dong.x/annotation/sequins
 input=/stornext/General/data/user_managed/grpu_mritchie_1/SCmixology/Mike_seqin/20200228_YPRDP_2xsequin_mixAB/sequins_rebasecall/pass/merged
 out=/stornext/General/data/user_managed/grpu_mritchie_1/SCmixology/Mike_seqin/20200228_YPRDP_2xsequin_mixAB/flames
-config=/wehisan/home/allstaff/d/dong.x/analysis/smchd1/long/scFLT/config_bulk_nanopore.json
+config=/wehisan/home/allstaff/d/dong.x/analysis/2020/sequins/flames/config_bulk_nanopore.json
 
-python $sftwr/sc_long_pipeline.py \
- --gff3 $anno/rnasequin_annotation_2.4.gtf\
- --infq $input\
- --outdir $out\
- --genomefa $anno/rnasequin_decoychr_2.4.fa\
- --minimap2 /wehisan/home/allstaff/d/dong.x/Programs/minimap2
+python $FLAMES/python/bulk_long_pipeline.py \
+ -a $anno/rnasequin_annotation_2.4.gtf\
+ -i $input\
+ -o $out\
+ -f $anno/rnasequin_decoychr_2.4.fa\
+ -c $config\
+ -m /wehisan/home/allstaff/d/dong.x/Programs/minimap2
