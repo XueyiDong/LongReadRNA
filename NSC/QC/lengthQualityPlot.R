f9ec7d1727fe12db7a7bee46671fc1f03eed3948
Xueyi Dong <dong.x@wehi.edu.au>
2021-03-01 22:25:24 +1100

    clean up NSC QC scripts

---
+++ NSC/QC/lengthQualityPlot.R
@@ -0,0 +1,30 @@
+library(ggplot2)
+
+qcdata <- readRDS("./summaryInfo.RDS")
+qcdata2 <- readRDS("./summaryInfo2.RDS")
+qcdata <- as.data.frame(qcdata, stringsAsFactors = FALSE)
+qcdata2 <- as.data.frame(qcdata2, stringsAsFactors = FALSE)
+qcdata <- rbind(qcdata, qcdata2)
+rm(qcdata2)
+qcdata$Read_length <- as.numeric(qcdata$Read_length)
+qcdata$Qscore <- as.numeric(qcdata$Qscore)
+
+maxLength = max(qcdata$Read_length)
+maxLength
+qcdata$LengthGroup <- Hmisc::cut2(qcdata$Read_length, cuts = c(0, 500, 1000,
+                                                     3000, maxLength))
+qcdata$LengthGroup <- gsub(" ", "", qcdata$LengthGroup)
+qcdata$LengthGroup <- gsub(",", ", ", qcdata$LengthGroup)
+
+qcdata$LengthGroup <- factor(qcdata$LengthGroup, levels = c(
+  "[0, 500)", "[500, 1000)", "[1000, 3000)", "[3000, 1353320]"
+))
+
+pdf("plots/lengthQualityViolin.pdf", height = 4, width = 8)
+ggplot(qcdata, aes(x=LengthGroup, y=Qscore, fill=LengthGroup, colour = LengthGroup)) +
+  geom_violin(alpha = 0.4) +
+  theme_bw() +
+  theme(text = element_text(size=20), legend.position = "none") +
+  labs(x = "Read length")
+dev.off()
+
--- NSC/QC/readNumPlot.R
+++ NSC/QC/readNumPlot.R
@@ -1,31 +1,5 @@
 library(ggplot2)
 library(scales)
-# readnum <- data.frame(
-#   number = c(19489349, 17668058, 13854515, 22164601, 6825392, 2657225, 18833786, 17433027, 13854516, 20677451, 5986242, 2686678),
-#   run = c(rep("run 1", 6), rep("run 2", 6)),
-#   quality = rep(rep(c("Pass", "Fail"), c(3, 3)), 2),
-#   category = factor(c(rep(c("raw", "demultiplexed",  "assigned"), 4)), levels= c("raw", "demultiplexed",  "assigned"))
-# )
-# pdf("totalreadnum.pdf", height = 4, width = 8)
-# ggplot(readnum, aes(x=category, y=number, fill=quality)) +
-#   geom_bar(stat = "identity") +
-#   facet_grid(.~run) +
-#   theme_bw() +
-#   geom_text(aes(label=number), position = position_stack(vjust = 0.5), size = 3)
-# dev.off()
-# 
-# readnum.ag <- aggregate(x=readnum$number, by=list(readnum$quality, readnum$category), FUN=sum)
-# colnames(readnum.ag) <- c("quality", "category", "number")
-# pdf("totalreadnum_combinedBatch.pdf", height = 4, width = 8)
-# ggplot(readnum.ag, aes(x=category, y=number, fill=quality)) +
-#   geom_bar(stat = "identity") +
-#   # facet_grid(.~run) +
-#   theme_bw() +
-#   geom_text(aes(label=number), position = position_stack(vjust = 0.5), size = 5) +
-#   theme(text = element_text(size=20)) +
-#   labs(x = "", y = "Number of reads")
-# dev.off()
-
 
 readnum <- data.frame(
   number = c(81165187, 57234663, 51135000, 37897462, 33311549),
